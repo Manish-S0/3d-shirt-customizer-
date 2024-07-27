d92a61a756713eeef27a716b5275310a12dbb06a
manish <manishkrsah88@gmail.com>
2024-07-27 14:08:12 +0545

    modify backdrop

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -15,23 +15,24 @@ const Backdrop = () => {
       scale={10}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0,0,-0.14]}>
+
         <RandomizedLight
           amount={8}
           radius={9}
-          intensity={0.95}
-          ambient={0.25}
+          intensity={1.95}
+          ambient={0.5}
           position={[5,5,-10]}
         
         />
+
         <RandomizedLight
           amount={4}
           radius={5}
           intensity={0.25}
-          ambient={0.25}
+          ambient={0.15}
           position={[-5,5,-5]}
-        
-        
         />
+
     </AccumulativeShadows>
   )
 }
